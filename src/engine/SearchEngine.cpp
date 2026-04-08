f3324c8ebfb3bcb22c8359f657bdc8265228510f
DennisThink-Windows <dennisthink@hotmail.com>
2026-04-08 17:33:01 +0800

    add code for calculate weight but failed

--- src/engine/SearchEngine.cpp
+++ src/engine/SearchEngine.cpp
@@ -11,6 +11,8 @@ GridType_T CSearchEngine::GetMove(const TicTacToeBoard_T& board,const ChessTypes
 	FillNode(newNode, side,side,0);
 
 	m_root = newNode;
+	CalculateAllNodes();
+	PrintAllMoveWays();
 	GridType_T result;
 	result._xPos = 0;
 	result._yPos = 0;
@@ -59,15 +61,115 @@ void CSearchEngine::PrintFillNode(Shared_TreeNode node)
 	{
 		CTicTacToeBoard board;
 		board.SetBoard(item->get()->m_board);
+		std::cout << " Value: " << item->get()->m_value << std::endl;
 		board.PrintBoard();
 	}
 	std::cout << "---------------Print Fill Node------------------" << std::endl;
+}
+
+bool CSearchEngine::CalculateValue(Shared_TreeNode node)
+{
+	ChessTypes_T otherSide = ChessTypes_T::None_Type;
+	if (node->m_playSide == ChessTypes_T::X_Type)
+	{
+		otherSide = ChessTypes_T::O_Type;
+	}
+	if (node->m_playSide == ChessTypes_T::O_Type)
+	{
+		otherSide = ChessTypes_T::X_Type;
+	}
+	CTicTacToeBoard board;
+	board.SetBoard(node->m_board);
+	if (board.IsSideWin(node->m_playSide))
+	{
+		node->m_value = MAX_VALUE;
+	}
+	else if (board.IsSideWin(otherSide))
+	{
+		node->m_value = MIN_VALUE;
+	}
+	else
+	{
+		node->m_value = DRAW_VALUE;
+	}
+	return false;
+}
+void CSearchEngine::PrintAllMoveWays()
+{
+	for (auto item : m_allLeaves)
+	{
+		std::cout << "---------------------PrintAllMoveWays-------------------------------" << std::endl;
+		PrintFillNode(item);
+		std::cout << "---------------------PrintAllMoveWays-------------------------------" << std::endl;
+	}
+}
+bool CSearchEngine::CalculateAllNodes()
+{
+	for (auto& item : m_allLeaves)
+	{
+		CalculateValue(item);
+	}
+
+	std::vector<Shared_TreeNode> oldNodes;
+	std::vector<Shared_TreeNode> newNodes;
+	oldNodes = m_allLeaves;
+	while(!oldNodes.empty())
+	{
+		for (auto& item : oldNodes)
+		{
+			Shared_TreeNode pParent = item->m_parent.lock();
+			if (pParent)
+			{
+				int maxValue = MIN_VALUE;
+				int nCount = 0;
+				for (auto& sonItem : pParent->m_sons)
+				{
+					if (sonItem->m_value != DEFAULT_VALUE)
+					{
+						maxValue = std::max(maxValue, sonItem->m_value);
+						nCount++;
+					}
+					else
+					{
+						break;
+					}
+				}
+				if (nCount == pParent->m_sons.size())
+				{
+					pParent->m_value = maxValue;
+					newNodes.push_back(pParent);
+				}
+				else
+				{
+					newNodes.push_back(pParent);
+				}
+			}
+		}
+		oldNodes = newNodes;
+		newNodes.clear();
+	}
+
+	return false;
 }
 bool CSearchEngine::FillNode(Shared_TreeNode node,const ChessTypes_T playSide, const ChessTypes_T side,const int depth)
 {
 	if (IsFillEnd(node, playSide, depth))
 	{
+		CalculateValue(node);
 		PrintFillNode(node);
+		bool bFind = false;
+		for (auto& item : m_allLeaves)
+		{
+			if (item.get()->m_board == node->m_board)
+			{
+				bFind = true;
+				break;
+			}
+		}
+		if (!bFind)
+		{
+			m_allLeaves.push_back(node);
+		}
 		return true;
 	}
 	else
@@ -102,6 +204,7 @@ bool CSearchEngine::FillNode(Shared_TreeNode node,const ChessTypes_T playSide, c
 					newNode->m_board = boardData;
 					newNode->m_parent = node;
 					newNode->m_depth = depth;
+					newNode->m_playSide = playSide;
 					node->m_sons.push_back(newNode);
 				}
 				
--- src/engine/SearchEngine.hpp
+++ src/engine/SearchEngine.hpp
@@ -5,11 +5,25 @@
 struct SearchTreeNode_T;
 using Shared_TreeNode = std::shared_ptr<SearchTreeNode_T>;
 using Weaked_TreeNode = std::weak_ptr<SearchTreeNode_T>;
+const static int DEFAULT_VALUE = -114514;
+const static int MIN_VALUE = -100;
+const static int DRAW_VALUE = 0;
+const static int MAX_VALUE = 100;
+
 struct SearchTreeNode_T
 {
+	SearchTreeNode_T() {
+		m_parent.reset();
+		m_depth = 0;
+		m_value = DEFAULT_VALUE;
+		m_playSide = ChessTypes_T::None_Type;
+		m_sons.clear();
+	}
 	Weaked_TreeNode m_parent;
 	int m_depth;
 	TicTacToeBoard_T m_board;
+	int m_value;
+	ChessTypes_T m_playSide;
 	std::vector<Shared_TreeNode> m_sons;
 };
 
@@ -21,8 +35,12 @@ private:
 	bool CreatNote(const TicTacToeBoard_T& board);
 	bool FillNode(Shared_TreeNode node,const ChessTypes_T playSide,const ChessTypes_T side,const int depth);
 	bool IsFillEnd(Shared_TreeNode node, const ChessTypes_T playSide, const int depth)const;
+	bool CalculateAllNodes();
+	void PrintAllMoveWays();
+	bool CalculateValue(Shared_TreeNode node);
 	void PrintFillNode(Shared_TreeNode node);
 	std::shared_ptr<SearchTreeNode_T> m_root;
+	std::vector<Shared_TreeNode> m_allLeaves;
 	std::vector<TicTacToeBoard_T> m_allBoards;
 };
 #endif
